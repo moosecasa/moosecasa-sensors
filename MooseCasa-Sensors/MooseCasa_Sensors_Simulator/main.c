4bce9500ce343f9205daa77068e9ffeac53dc5eb
James <james2432@gmail.com>
2019-11-20 19:35:28 -0500

    #1 Project Structure
    -Added project base structure for atmel studio.
    -Updated gitignore

---
+++ MooseCasa-Sensors/MooseCasa_Sensors_Simulator/main.c
@@ -0,0 +1,18 @@
+/*
+ * MooseCasa_Sensors_Target.c
+ *
+ * Created: 11/20/2019 5:10:50 PM
+ * Author : Jamie Nadeau
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
